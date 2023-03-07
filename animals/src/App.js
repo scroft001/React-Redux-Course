75ac84a042a0cb52f4e60f2c3187ad9aeb2397b1
Steven Croft <scroft001@gmail.com>
2023-03-06 21:25:33 -0800

    started animal show component

--- animals/src/App.js
+++ animals/src/App.js
@@ -2,9 +2,14 @@
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={handleClick}>Add Animal</button>
     </div>
   );
 }
